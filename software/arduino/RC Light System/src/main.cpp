60eb6a74d4c406192adff48dc9fb0b61e5bf6f2f
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2023-11-26 02:02:08 +0100

    Works some of the time, readings are glitched in some way

--- software/arduino/RC Light System/src/main.cpp
+++ software/arduino/RC Light System/src/main.cpp
@@ -10,6 +10,15 @@
 #define PIN_AUX 18
 #define PIN_VOL 21
 
+#define LED_FL_A 0
+#define LED_FL_B 1
+#define LED_BR_A 2
+#define LED_BR_B 3
+#define LED_BL_L 4
+#define LED_BL_R 5
+#define LED_EX_A 6
+#define LED_EX_B 7
+
 uint8_t led_state = 0x00;
 
 long thr_value = 0;
@@ -66,15 +75,13 @@ void clear_channels() {
   write_channels();
 }
 
-bool get_channel(uint8_t number) {
-  return bitRead(led_state, number) == 1;
-}
-
 void set_channel(uint8_t number, bool value = true) {
   uint8_t old_state = led_state;
   if (value) bitSet(led_state, number);
   else bitClear(led_state, number);
-  channels_dirty = (old_state == led_state);
+  if (old_state == led_state) {
+    channels_dirty = true;
+  }
 }
 
 
@@ -118,6 +125,7 @@ bool values_seen() {
 #define THR_STICK_IDLE 0
 #define THR_STICK_LOW 1
 #define THR_STICK_HIGH 2
+
 int get_thr_state() {
   if (thr_value > 1800) return THR_STICK_HIGH;
   if (thr_value > 1600) return THR_STICK_LOW;
@@ -126,29 +134,101 @@ int get_thr_state() {
   return THR_STICK_IDLE;
 }
 
+int last_thr_state = THR_STICK_IDLE;
+unsigned long last_thr_event = 0;
+int get_debounced_thr_state() {
+  int new_state = get_thr_state();
+
+  if (last_thr_state != new_state) {
+    long duration = millis() - last_thr_event;
+    if ((duration) > 250) {
+      last_thr_state = new_state;
+      last_thr_event = millis();
+
+      Serial.print("THR set to ");
+      Serial.print(last_thr_state);
+      Serial.print(" ");
+      Serial.println(duration);
+    }
+  }
+  return last_thr_state;
+}
+
+#define STR_STICK_LEFT -1
+#define STR_STICK_IDLE 0
+#define STR_STICK_RIGHT 1
+int get_str_state() {
+  if (str_value > 1640) return STR_STICK_RIGHT;
+  if (str_value < 1450) return STR_STICK_LEFT;
+  return STR_STICK_IDLE;
+}
+
+void on_idle() {
+  switch (get_str_state()) {
+    case STR_STICK_LEFT:
+      set_channel(LED_BL_L, true);
+      set_channel(LED_BL_R, false);
+      break;
+
+    case STR_STICK_RIGHT:
+      set_channel(LED_BL_L, false);
+      set_channel(LED_BL_R, true);
+      break;
+
+    default:
+    case STR_STICK_IDLE:
+      set_channel(LED_BL_L, false);
+      set_channel(LED_BL_R, false);
+      break;
+  }
+}
+
+void on_control() {
+  switch (get_debounced_thr_state()) {
+    case THR_STICK_REVERSE_HIGH:
+      set_channel(LED_FL_A, false);
+      set_channel(LED_FL_B, false);
+      set_channel(LED_BR_A, true);
+      set_channel(LED_BR_B, true);
+      break;
+
+    case THR_STICK_REVERSE_LOW:
+      set_channel(LED_FL_A, false);
+      set_channel(LED_FL_B, false);
+      set_channel(LED_BR_A, true);
+      set_channel(LED_BR_B, false);
+      break;
+
+    case THR_STICK_HIGH:
+      set_channel(LED_FL_A, true);
+      set_channel(LED_FL_B, true);
+      set_channel(LED_BR_A, false);
+      set_channel(LED_BR_B, false);
+      break;
+
+    case THR_STICK_LOW:
+      set_channel(LED_FL_A, true);
+      set_channel(LED_FL_B, false);
+      set_channel(LED_BR_A, false);
+      set_channel(LED_BR_B, false);
+      break;
+
+    case THR_STICK_IDLE:
+    default:
+      set_channel(LED_FL_A, false);
+      set_channel(LED_FL_B, false);
+      set_channel(LED_BR_A, false);
+      set_channel(LED_BR_B, false);
+      on_idle();
+      break;
+    }
+}
+
 void loop() {
   update_values();
 
   if (values_seen()) {
-    switch (get_thr_state()) {
-    case THR_STICK_REVERSE_HIGH:
-    case THR_STICK_HIGH:
-      set_channel(0, true);
-      set_channel(1, true);
-      break;
-
-    case THR_STICK_REVERSE_LOW:
-    case THR_STICK_LOW:
-      set_channel(0, true);
-      set_channel(1, false);
-      break;
-
-    default:
-      set_channel(0, false);
-      set_channel(1, false);
-      break;
-    }
-
+    on_control();
     sync_channels();
   }
 }
